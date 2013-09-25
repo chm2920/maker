69aa0c002d07837ac24fa2aae001e09f59bc3839
pathfinder <chm2920@gmail.com>
2013-09-26 00:19:58 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maker::Application.config.secret_token = '8533668e6b7b3caef583e5326e66071eac469f7ed4eb21cf318d6015970487d40d1d0865e454e5695a155ae99f35ea2f195534aba22b2670bb1878383081a6cb'
