c25a3f7560defdc144eeb49894823c39ea3579f7
pathfinder <chm2920@gmail.com>
2013-09-28 14:29:04 +0800

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def format_content(content)
+    # (simple_format content).gsub("<br>", "").gsub("<br />", "").gsub("<br/>", "").html_safe
+    content.html_safe
+  end
+  
 end
