38a72e9fbbd45607825264fd1d89e12faf6f98dc
pathfinder <chm2920@gmail.com>
2013-10-08 21:04:03 +0800

    update

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -3,7 +3,7 @@ class StartController < ApplicationController
   before_filter :find_catalogs
   
   def index
-    @topics = Topic.paginate :page => params[:page], :per_page => 10, :conditions => "is_trash = 0", :order => "id desc"
+    @topics = Topic.paginate :page => params[:page], :per_page => 8, :conditions => "is_trash = 0", :order => "id desc"
     @friendlinks = Friendlink.find(:all, :order => "rank asc")
   end
   
@@ -13,7 +13,7 @@ class StartController < ApplicationController
       redirect_to "/"
       return
     end
-    @topics = Topic.paginate :page => params[:page], :per_page => 10, :conditions => ["catalog_id = ? and is_trash = 0", @catalog.id], :order => "id desc"
+    @topics = Topic.paginate :page => params[:page], :per_page => 8, :conditions => ["catalog_id = ? and is_trash = 0", @catalog.id], :order => "id desc"
   end
   
   def tag
@@ -22,7 +22,7 @@ class StartController < ApplicationController
       redirect_to "/"
       return
     end
-    @topics = @tag.topics.paginate :page => params[:page], :per_page => 10, :conditions => ["is_trash = 0"], :order => "id desc"
+    @topics = @tag.topics.paginate :page => params[:page], :per_page => 8, :conditions => ["is_trash = 0"], :order => "id desc"
   end
   
   def show
